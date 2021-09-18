170042c42f758a4acddc1fc695856fcd05940ed6
toliz <apospana@ece.auth.gr>
2021-09-18 11:41:01 +0200

    Added solution for 1.3

--- Lab 1/photometric/construct_surface.py
+++ Lab 1/photometric/construct_surface.py
@@ -2,43 +2,54 @@ import numpy as np
 
 
 def construct_surface(p, q, path_type='column'):
-    '''
-    CONSTRUCT_SURFACE construct the surface function represented as height_map
-       p : measures value of df/dx
-       q : measures value of df/dy
-       path_type: type of path to construct height_map, either 'column',
-       'row', or 'average'
-       height_map: the reconstructed surface
-    '''
+    # CONSTRUCT_SURFACE construct the surface function represented as height_map
+    # p : measures value of df/dx
+    # q : measures value of df/dy
+    # path_type: type of path to construct height_map, either 'column',
+    # 'row', or 'average'
+    # height_map: the reconstructed surface
 
     h, w = p.shape
     height_map = np.zeros([h, w])
 
     if path_type == 'column':
-        """
-        ================
-        Your code here
-        ================
-        % top left corner of height_map is zero
-        % for each pixel in the left column of height_map
-        %   height_value = previous_height_value + corresponding_q_value
-        %
-        % for each row
-        %   for each element of the row except for leftmost
-        %       height_value = previous_height_value + corresponding_p_value
+        for i in range(1, h):
+            height_map[i, 0] = height_map[i-1, 0] + q[i, 0]
 
-        """
+        for i in range(h):
+            for j in range(1, w):
+                height_map[i, j] = height_map[i, j-1] + p[i, j]
+
     elif path_type == 'row':
-        """
-        ================
-        Your code here
-        ================
-        """
+        for j in range(1, w):
+            height_map[0, j] = height_map[0, j-1] + p[0, j]
+
+        for j in range(w):
+            for i in range(1, h):
+                height_map[i, j] = height_map[i-1, j] + q[i, j]
+                
     elif path_type == 'average':
-        """
-        ================
-        Your code here
-        ================
-        """
+        # height map by integrating by columns
+        height_map_col = np.zeros([h, w])
+
+        for i in range(1, h):
+            height_map_col[i, 0] = height_map_col[i-1, 0] + q[i, 0]
+
+        for i in range(h):
+            for j in range(1, w):
+                height_map_col[i, j] = height_map_col[i, j-1] + p[i, j]
+        
+        # height map by integrating by rows
+        height_map_row = np.zeros([h, w])
+
+        for j in range(1, w):
+            height_map_row[0, j] = height_map_row[0, j-1] + p[0, j]
+
+        for j in range(w):
+            for i in range(1, h):
+                height_map_row[i, j] = height_map_row[i-1, j] + q[i, j]
+
+        # average
+        height_map = (height_map_col + height_map_row) / 2
 
     return height_map
