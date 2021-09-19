68e2338f759c143a333177b9e155b745e2c7c854
Sameer Ambekar <ambekarsameer@gmail.com>
2021-09-19 21:37:42 +0200

    minor changes

--- Lab 1/colour_constancy/awb.py
+++ Lab 1/colour_constancy/awb.py
@@ -3,16 +3,10 @@ import numpy as np
 
 img = cv2.imread('awb.jpg')
 
+img= img.transpose(2, 0, 1).astype(np.uint32)
+ab = np.mean(img[1])
+img[0] = np.minimum(img[0] * (ab / np.mean(img[0])), 255)
+img[2] = np.minimum(img[2] * (ab / np.mean(img[2])), 255)
+img = img.transpose(1, 2, 0).astype(np.uint8)
 
-
-def grey_world(nimg):
-    nimg = nimg.transpose(2, 0, 1).astype(np.uint32)
-    mu_g = np.average(nimg[1])
-    nimg[0] = np.minimum(nimg[0] * (mu_g / np.average(nimg[0])), 255)
-    nimg[2] = np.minimum(nimg[2] * (mu_g / np.average(nimg[2])), 255)
-    return nimg.transpose(1, 2, 0).astype(np.uint8)
-
-gray = grey_world(img)
-#transponse using library now 
-#gray = cv2.cvtColor(gray, cv2.COLOR_BGR2RGB)
-cv2.imwrite('gray_world_image.jpg', gray)
+cv2.imwrite('gray_world_image2.jpg', img)
