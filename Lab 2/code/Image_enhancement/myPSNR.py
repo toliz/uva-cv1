b12e6e2a200d63bf1442cd133468349a43e61710
toliz <apospana@ece.auth.gr>
2021-09-26 16:57:51 +0200

    Added solution for Question 6

--- Lab 2/code/Image_enhancement/myPSNR.py
+++ Lab 2/code/Image_enhancement/myPSNR.py
@@ -1,3 +1,26 @@
-def myPSNR( orig_image, approx_image ):
-    print('Not implemented\n')
+import cv2
+import numpy as np
+
+
+def myPSNR(orig_image, approx_image):
+    assert orig_image.shape == approx_image.shape, "please use images of the same shape"
+
+    # convert pixels to float values
+    if orig_image.dtype != np.dtype('float32'):
+        orig_image = orig_image.astype('float32')
+
+    if approx_image.dtype != np.dtype('float32'):
+        approx_image = approx_image.astype('float32')
+
+    # compute RMSE
+    RMSE = np.sqrt(np.mean( (orig_image - approx_image) ** 2 ))
+    PSNR = 20 * np.log10(orig_image.max() / RMSE)
+    
     return PSNR
+
+
+if __name__ == '__main__':
+    orig_image = cv2.imread('./images/image1.jpg')
+    approx_image = cv2.imread('./images/image1_gaussian.jpg')
+
+    print(f'PSNR = {myPSNR(orig_image, approx_image)} dB.')
